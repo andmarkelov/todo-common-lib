20130f4cad38de4810dc268e92bfeb445baef46f
wmrk <andrew.wamark@gmail.com>
2022-09-08 14:02:09 +0300

    refactoring

---
+++ src/main/java/com/wmrk/todo/jwt/Role.java
@@ -0,0 +1,19 @@
+package com.wmrk.todo.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@RequiredArgsConstructor
+public enum Role implements GrantedAuthority {
+
+    ROLE_ADMIN("ADMIN"),
+    ROLE_USER("USER");
+
+    private final String vale;
+
+    @Override
+    public String getAuthority() {
+        return vale;
+    }
+
+}
